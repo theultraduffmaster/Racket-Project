01fcf7c7e6c35a083a9c77efaa580d6e6570a8e8
theultraduffmaster <g00318025@gmit.ie>
2017-02-18 17:13:34 +0000

    Cleanup of repository

--- Countdown-solution.rkt
+++ Countdown-solution.rkt
@@ -1,13 +1,17 @@
 #lang racket
 
+;4 sets of random small numbers are defined here for use later on
+;all the small numbers are set between 1 and 11 so that 10 can be used as the random function takes in (a b) but returns b-1 as the biggest number
+;a will still return 1 fine
 (define S1(random 1 11))
 (define S2(random 1 11))
 (define S3(random 1 11))
 (define S4(random 1 11))
 
+;T is our target number and thus has to be a random number between 101 and 999 (1000-1 allows us to have 999) inclusive
+(define T(random 101 1000))
 
-(define T(random 101 999))
-
+;Calling all our defined functions here so we can see some ouput so far
 S1
 S2
 S3
